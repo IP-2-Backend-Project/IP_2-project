5d0ee70c2d63225bf26427a87a97e68650dad641
semehal62 <semehalhailu4@gmail.com>
2026-03-22 19:18:47 +0300

    initial commit(Updated the README and created the database

---
+++ Database.sql
@@ -0,0 +1,21 @@
+-- 1. Create the Database
+CREATE DATABASE company_db;
+USE company_db;
+
+-- 2. Users Table (For Login)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'applicant') DEFAULT 'applicant'
+);
+
+-- 3. Applications Table (To track who is accepted)
+CREATE TABLE applications (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    position_name VARCHAR(100) NOT NULL,
+    status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+    applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
---
+++ README.md
@@ -0,0 +1,7 @@
+# Name - ID- Github username 
+***
+# Sosina Girmay - ETS1311/16 -Sosina47
+# Soliyana Wasyihun -ETS1304/16  - 19souly97
+# Shegitu Shukeri - ETS1277/16 - Shegitu
+# Sifen Tesfaye - ETS1282/16 - Sifen-tesfaye
+# Semehal Hailu - ETS1266/16 - semehal62
