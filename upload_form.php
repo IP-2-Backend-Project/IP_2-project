e10abc35988355b46a92229924b203c231648ba2
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-03-30 13:29:30 +0300

    Create upload_form.php

---
+++ upload_form.php
@@ -0,0 +1,10 @@
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+
+    <label>Select file:</label>
+    <input type="file" name="file" required>
+
+    <button type="submit">
+        Upload
+    </button>
+
+</form>
