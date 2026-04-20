bdc0ff6bfeccd5eb05f8a75206f8ec12bdbeab69
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2026-04-21 01:26:32 +0300

    added user login check
    Added user login check before role validation.

--- api/applications/update_status.php
+++ api/applications/update_status.php
@@ -1,8 +1,19 @@
+<?php
 session_start();
+
+// check if the user is logged in
+if (!isset($_SESSION["user_id"])) {
+    echo "User not logged in";
+    exit();
+}
 
 if ($_SESSION["role"] != "Recruiter") {
 
     echo "Access denied!";
     exit();
-
 }
+
+
+?>
+
+
