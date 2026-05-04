71a9bf1abcef4ad8795026c02c5ffe94dd190875
soliyana wasiyhun <soliyana.wasiyhun@aastustudent.edu.et>
2026-05-04 01:40:41 -0700

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,7 @@ session_start();
 require 'config/db.php'; 
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: frontend/login.php");
+header("Location: http://localhost:5173");
     exit();
 }
 echo "Welcome, " . htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8');
