49b4cb61b6f1c071f50cb7713613a42f5dfaabe2
soliyana wasiyhun <soliyana.wasiyhun@aastustudent.edu.et>
2026-05-03 23:04:09 -0700

    Add CORS headers for API requests

--- api/cors.php
+++ api/cors.php
@@ -2,3 +2,7 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, X-CSRF-Token');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') { http_response_code(204); exit; }
+session_start();
