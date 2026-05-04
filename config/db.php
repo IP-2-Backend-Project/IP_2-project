657b511c7402869d89b0659b104d0129ec991a6d
soliyana wasiyhun <soliyana.wasiyhun@aastustudent.edu.et>
2026-05-04 02:21:20 -0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,29 +1,17 @@
 <?php
-// Configuration 
-$host    = "localhost";
-$db      = "ip_project";
-$user    = "root";
-$pass    = "";
-$charset = "utf8mb4";
+$host = "localhost";
+$dbname = "ip_project";
+$username = "root";
+$password = "";
 
-// TData Source 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-// Connection Options
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
-    PDO::ATTR_EMULATE_PREPARES   => false                 
-];
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-
-    header('Content-Type: application/json');
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
     echo json_encode([
         "status" => "error",
-        "message" => "Database connection failed: " . $e->getMessage()
+        "message" => "Database connection failed"
     ]);
-    exit;
+    exit();
 }
 ?>
