d590cce86dd4d41827dbdd3493889f4c3b0275d4
soliyana wasiyhun <soliyana.wasiyhun@aastustudent.edu.et>
2026-05-03 23:05:36 -0700

    Update login.php

--- api/login.php
+++ api/login.php
@@ -1,1 +1,9 @@
+<?php
+require_once '../config/db.php'; // your existing PDO connection
+require_once 'cors.php';
 
+$data = json_decode(file_get_contents('php://input'), true);
+$email = $data['email'] ?? '';
+$password = $data['password'] ?? '';
+
+$stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
